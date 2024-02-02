5ff8d36476c375c05bfe169db090d15ef86c307b
Thomas Stauffer <thomasstauffer@gmx.ch>
2024-02-02 10:41:39 +0100

    BLEAdvertising: adjust name

--- BLEAdvertising/BLEAdvertising.ino
+++ BLEAdvertising/BLEAdvertising.ino
@@ -5,12 +5,12 @@
 BLEAdvertising* pAdvertising = nullptr;
 
 void setup() {
-  BLEDevice::init("ESP32-FireFly");
+  BLEDevice::init("ESP32-FireFly-Advertising");
   BLEServer* pServer = BLEDevice::createServer();
   pAdvertising = pServer->getAdvertising();
 
   BLEAdvertisementData advertisementData;
-  advertisementData.setName("ESP32-FireFly");
+  advertisementData.setName("ESP32-FireFly-Advertising");
   pAdvertising->setAdvertisementData(advertisementData);
 }
 
@@ -19,7 +19,7 @@ int counter = 0;
 void loop() {
   BLEAdvertisementData scanResponseData;
   // 2 bytes manufaturer ID (0x02e5 = Espressif), the rest custom
-  char scanResponse[4] = {0xe5, 0x02, (counter % 2) == 0 ? 0x01 : 0x02, 0x00};
+  char scanResponse[4] = { 0xe5, 0x02, (counter % 2) == 0 ? 0x01 : 0x02, 0x00 };
   scanResponseData.setManufacturerData(scanResponse);
 
   pAdvertising->stop();
